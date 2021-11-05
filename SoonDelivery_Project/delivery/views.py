6ff5dc37b9dccbded4534b77c24a6f505cc116c9
tjdgus <tjdgus1977@gmail.com>
2021-11-05 18:12:01 +0900

    [BE] :: 회원가입 오류 및 사용자 권한 설정

--- SoonDelivery_Project/delivery/views.py
+++ SoonDelivery_Project/delivery/views.py
@@ -10,33 +10,47 @@ def home(request):
     order_list = delivery_info.objects.filter(is_delivered=0)
     return render(request, 'main.html', {'order_list':order_list})
 
-def order(request, user_id):
-    if request.method == 'POST':
-        new_order = delivery_info()
-        new_order.ordered_time = timezone.now()
-        new_order.delivery_location = request.POST["delivery_location"]
-        new_order.delivery_price = request.POST["delivery_price"]
-        new_order.delivery_list = request.POST["delivery_list"]
-        new_order.delivery_owner = User.objects.get(id=user_id)
-        new_order.stuff_price = request.POST["stuff_price"]
-        new_order.extra_order = request.POST["extra_order"]
-        new_order.store_location = request.POST["store_location"]
-        new_order.is_delivered = 0
-        new_order.save()
-        return redirect('home')
-    return render(request, 'order.html')
+def order(request, user_id=0):
+    if user_id == 0:
+        redirect('login')
+    try:
+        if request.method == 'POST':
+            new_order = delivery_info()
+            new_order.ordered_time = timezone.now()
+            new_order.delivery_location = request.POST["delivery_location"]
+            new_order.delivery_price = request.POST["delivery_price"]
+            new_order.delivery_list = request.POST["delivery_list"]
+            new_order.delivery_owner = User.objects.get(id=user_id)
+            new_order.stuff_price = request.POST["stuff_price"]
+            new_order.extra_order = request.POST["extra_order"]
+            new_order.store_location = request.POST["store_location"]
+            new_order.is_delivered = 0
+            new_order.save()
+            return redirect('home')
+        return render(request, 'order.html')
+    except ValueError:
+            return redirect('login')
 
 def order_delivery(request, order_id):
     order_list = get_object_or_404(delivery_info, pk=order_id)
     name = order_list.delivery_owner
     return render(request, 'delivery.html', {'order_list':order_list, 'order_id': order_id, 'name':name})
 
-def start_delivery(request, user_id, order_id):
-    order_detail = delivery_info.objects.get(id=order_id)
-    order_detail.time_required = 0
-    user = User.objects.get(id=user_id)
-    order_detail.delivery_man = user
-    order_detail.is_delivered = 1
-    order_detail.save()
-    nickname = order_detail.delivery_owner.nickname
-    return render(request, 'start_delivery.html', {'order_detail':order_detail, 'nickname':nickname})
+def start_delivery(request, user_id=0, order_id=0):
+    if user_id == 0:
+        redirect('login')
+    try:
+        details = get_object_or_404(User, id=user_id)
+        if details.is_trial == 0:
+            return render(request, 'school_email_page.html')
+        order_detail = delivery_info.objects.get(id=order_id)
+        order_detail.time_required = 0
+        user = User.objects.get(id=user_id)
+        order_detail.delivery_man = user
+        order_detail.is_delivered = 1
+        order_detail.save()
+        nickname = order_detail.delivery_owner.nickname
+        return render(request, 'start_delivery.html', {'order_detail':order_detail, 'nickname':nickname})
+    except ValueError:
+            return redirect('login')
+
