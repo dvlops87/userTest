4881faf7033c75af85e1ae353019a59d161eec09
tjdgus <tjdgus1977@gmail.com>
2021-11-18 18:54:35 +0900

    python

--- SoonDelivery_Project/SoonDelivery_Project/settings.py
+++ SoonDelivery_Project/SoonDelivery_Project/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*pdw9n)m1skp&^+ipjn&(+3*7%pt2@#&^g!v%!x#n=l-^-0y9t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'hyun1011.pythonanywhere.com'
+]
 
 
 # Application definition
@@ -122,9 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
