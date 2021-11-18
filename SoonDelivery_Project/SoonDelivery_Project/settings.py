e89763f6ef823103014f05c0f647a2260ab30a94
tjdgus <tjdgus1977@gmail.com>
2021-11-18 21:32:20 +0900

    1

--- SoonDelivery_Project/SoonDelivery_Project/settings.py
+++ SoonDelivery_Project/SoonDelivery_Project/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*pdw9n)m1skp&^+ipjn&(+3*7%pt2@#&^g!v%!x#n=l-^-0y9t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'hyun1011.pythonanywhere.com'
+]
 
 
 # Application definition
